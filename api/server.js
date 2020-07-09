e66f95ec9083a3414a40f87f09469cc8d9c8bd40
Brian Hague <alphaseinor@gmail.com>
2020-07-09 18:56:16 -0500

    added a route to root

--- api/server.js
+++ api/server.js
@@ -1,11 +1,18 @@
+require('dotenv').config()
 const express = require("express")
 const cors = require("cors")
-const helmet = require("helmet")
+const helmet = require("helmet");
+const { response } = require('express');
 
 const server = express();
 
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
+
+server.use('/', ()=>{
+  console.log(`${process.env.NAME}'s Server listening on port ${PORT}`)
+  res.staus(200).json({message: `${process.env.NAME}'s Server listening on port ${PORT}`})
+})
 
 module.exports = server
